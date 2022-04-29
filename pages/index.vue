d33c5d474fe4689b12b007bcb62cf3130b419906
Dewey Ou <oushihao97@gmail.com>
2022-04-29 17:13:57 +0800

    feat: title style

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,22 @@ import { compares } from '~/composables/compares'
     pb-4
     mb-6
     border="b-1 gray-500/20"
+    flex="~ col"
+    justify-center
+    items-center
   >
-    Vue to React
+    <img src="/logo.svg" w-30 h-30 mb-3>
+    <h1>Vue to React</h1>
+    <a
+      class="icon-btn"
+      mt-3
+      text="xl center"
+      i-carbon-logo-github
+      rel="noreferrer"
+      href="https://github.com/ouduidui/vue-to-react"
+      target="_blank"
+      title="GitHub"
+    />
   </div>
   <CompareItem
     v-for="compare in compares"
